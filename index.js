ff76486fd5839fa8ce7d9f7a2922b7f4bf5b12f1
mfuku <mfukushima@mbn.nifty.com>
2021-01-15 22:05:12 +0900

    fix Windows (Japanese Edition) problem

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { exec } = require('child_process');
 const macLookup = require('./macLookup.js');
 
 var flag,
-    ipCommand,
-    osType = os.type();
+  ipCommand,
+  osType = os.type();
 
 switch(osType) {
     case 'Windows_NT':
         flag = '-w';
-        ipCommand = 'ipconfig';
+        ipCommand = 'chcp 65001&&ipconfig /all';
         break;
     case 'Linux':
         flag = '-w';
@@ -40,38 +40,46 @@ function Arpping({ timeout = 5, includeEndpoints = false, useCache = true, cache
 }
 
 /**
-* Build array of full ip range (xxx.xxx.x.1-255) given example ip address
-* @param {String} ip
-*/
+ * Build array of full ip range (xxx.xxx.x.1-255) given example ip address
+ * @param {String} ip
+ */
 Arpping.prototype._getFullRange = function(ip) {
     // don't use default assignment so false-y values are overwritten
     ip = ip || this.myIP;
     var ipStart = ip.substr(0, ip.lastIndexOf('.') + 1);
-    return this.includeEndpoints ? 
-        Array.from({ length: 255 }, (_, i) => ipStart + (i + 1)):
-        Array.from({ length: 253 }, (_, i) => ipStart + (i + 2));
+    return this.includeEndpoints ?
+      Array.from({ length: 255 }, (_, i) => ipStart + (i + 1)):
+      Array.from({ length: 253 }, (_, i) => ipStart + (i + 2));
 }
 
 /**
-* Find ip and mac addresses of host device
-*/
+ * Find ip and mac addresses of host device
+ */
 Arpping.prototype.findMyInfo = function() {
     return new Promise((resolve, reject) => {
         exec(ipCommand, (err, stdout, stderr) => {
             if (err) return reject(err);
 
             var output = null;
+            var ip = null;
+            var mac = null;
             if (osType == 'Linux') {
                 if (stdout.indexOf('wlan0') == -1) return reject(new Error('No wifi connection'));
                 output = stdout.split('wlan0')[1];
+                ip = output.slice(output.indexOf('inet ') + 5, output.indexOf(' netmask')).trim();
+            } else if(osType == 'Windows_NT') {
+                ip = stdout.slice(stdout.indexOf('IPv4 Address'), stdout.indexOf('Subnet Mask')).trim();
+                ip = ip.slice(ip.indexOf(':')+1, ip.indexOf('(Preferred)')).trim();
+                mac = stdout.slice(stdout.indexOf('Physical Address'),stdout.indexOf('DHCP Enabled')).trim();
+                mac = mac.slice(mac.indexOf(':')+1).trim();
             }
             else {
                 output = stdout.slice(stdout.indexOf('en0'));
                 output = output.slice(0, output.indexOf('active\n')) + 'active';
                 if (output.split('status: ')[1] == 'inactive') return reject(new Error('No wifi connection'));
+                ip = output.slice(output.indexOf('IPv4 Address ') + 5, output.indexOf('Subnet Mask')).trim();
+                mac = output.slice(output.indexOf('ether ')).split('\n')[0].split(' ')[1].trim().replaceAll('-',':');
             }
-            var ip = output.slice(output.indexOf('inet ') + 5, output.indexOf(' netmask')).trim();
-            var mac = output.slice(output.indexOf('ether ')).split('\n')[0].split(' ')[1].trim();
             var type = macLookup(mac);
 
             this.myIP = ip;
@@ -81,10 +89,10 @@ Arpping.prototype.findMyInfo = function() {
 }
 
 /**
-* Discover all hosts connected to your local network or based on a reference IP address
-* @param {String} refIP
-* @param {Boolean} retry
-*/
+ * Discover all hosts connected to your local network or based on a reference IP address
+ * @param {String} refIP
+ * @param {Boolean} retry
+ */
 Arpping.prototype.discover = function(refIP, retry = true) {
     if (this.useCache && this.cache.length && Date.now() - this.cacheUpdate < this.cacheTimeout * 1000) {
         return new Promise((resolve, reject) => resolve(this.cache));
@@ -103,9 +111,9 @@ Arpping.prototype.discover = function(refIP, retry = true) {
 }
 
 /**
-* Ping a range of ip addresses
-* @param {Array} range
-*/
+ * Ping a range of ip addresses
+ * @param {Array} range
+ */
 Arpping.prototype.ping = function(range) {
     if (!(Array.isArray(range) && range.length)) {
         if (!this.myIP) return this.findMyInfo().then(() => this.ping(range));
@@ -114,14 +122,14 @@ Arpping.prototype.ping = function(range) {
 
     return new Promise((resolve, reject) => {
         var hosts = [],
-            missing =[],
-            checked = 0;
+          missing =[],
+          checked = 0;
         range.forEach(ip => {
             exec(`ping ${flag} ${this.timeout} ${ip}`, (err, stdout, stderr) => {
                 checked++;
                 if (err || stdout.indexOf('100% packet loss') > -1) missing.push(ip);
                 else hosts.push(ip);
-                
+
                 if (checked == range.length) resolve({ hosts, missing });
             });
         });
@@ -129,36 +137,40 @@ Arpping.prototype.ping = function(range) {
 }
 
 /**
-* Arp a range of ip addresses
-* @param {Array} range
-*/
+ * Arp a range of ip addresses
+ * @param {Array} range
+ */
 Arpping.prototype.arp = function(range) {
     return new Promise((resolve, reject) => {
         if (typeof range == 'string') range = [ range ];
         else if (!Array.isArray(range)) return reject(new Error('range must be an array of IP addresses'));
         else if (!range.length) return resolve({ hosts: [], missing: [] });
-
+        //  reference https://github.com/telemansoft/arpping
+        var command = (osType === 'Linux' || osType === 'Darwin') ? 'arp ' : 'arp -a ';
         var hosts = [],
-            missing = [],
-            checked = 0;
+          missing = [],
+          checked = 0;
         range.forEach(ip => {
-            exec(`arp ${ip}`, (err, stdout, stderr) => {
+            exec(`${command}${ip}`, (err, stdout, stderr) => {
                 checked++;
-                if (err || stdout.indexOf('no entry') > -1) missing.push(ip);
+                if (err || stdout.toLowerCase().indexOf('no') > -1) missing.push(ip);
                 else {
-                    var mac = (osType == 'Linux') ? 
-                        stdout.split('\n')[1].replace(/ +/g, ' ').split(' ')[2]: 
-                        stdout.split(' ')[3];
-                    if (mac.includes('incomplete')) missing.push(ip);
-                    else {
-                        var host = { ip, mac };
-                        var type = macLookup(mac);
-                        if (type) host.type = type;
-                        if (ip == this.myIP) host.isHostDevice = true;
-                        hosts.push(host);
+                    //  reference https://github.com/telemansoft/arpping
+                    var mac = ""
+                    if (osType === 'Linux') {
+                        mac = stdout.split('\n')[1].replace(/ +/g, ' ').split(' ')[2];
+                    } else if (osType === 'Darwin') {
+                        mac = stdout.split(' ')[3];
+                    } else {
+                        mac = (stdout.split('\n')[3] || '').replace(/ +/g, ' ').replace(/-/g, ':').split(' ')[2];
                     }
+                    var known = macLookup(mac);
+                    var host = {mac,ip};
+                    if (known) host.type = known;
+                    if (ip == this.myIP) host.isHostDevice = true;
+                    hosts.push(host);
                 }
-                
+
                 if (checked == range.length) resolve({ hosts, missing });
             });
         });
@@ -166,10 +178,10 @@ Arpping.prototype.arp = function(range) {
 }
 
 /**
-* Search for one or multiple IP addresses
-* @param {String/Array} ipArray
-* @param {String} refIP
-*/
+ * Search for one or multiple IP addresses
+ * @param {String/Array} ipArray
+ * @param {String} refIP
+ */
 Arpping.prototype.searchByIpAddress = function(ipArray, refIP) {
     if (typeof ipArray === 'string') ipArray = [ ipArray ];
     else if (!Array.isArray(ipArray) || !ipArray.length) {
@@ -179,17 +191,17 @@ Arpping.prototype.searchByIpAddress = function(ipArray, refIP) {
     return this.discover(refIP || ipArray[0]).then(hosts => {
         var hostIPs = hosts.map(h => h.ip);
         return {
-            hosts: hosts.filter(h => ipArray.includes(h.ip)), 
+            hosts: hosts.filter(h => ipArray.includes(h.ip)),
             missing: ipArray.filter(ip => !hostIPs.includes(ip))
         }
     });
 }
 
 /**
-* Search for one or multiple, full or partial mac addresses
-* @param {String/Array} macArray
-* @param {String} refIP
-*/
+ * Search for one or multiple, full or partial mac addresses
+ * @param {String/Array} macArray
+ * @param {String} refIP
+ */
 Arpping.prototype.searchByMacAddress = function(macArray, refIP) {
     if (typeof macArray == 'string') macArray = [ macArray ];
     else if (!Array.isArray(macArray) || !macArray.length) {
@@ -210,10 +222,10 @@ Arpping.prototype.searchByMacAddress = function(macArray, refIP) {
 }
 
 /**
-* Search for devices with the designated mac address type
-* @param {String} macType
-* @param {String} refIP
-*/
+ * Search for devices with the designated mac address type
+ * @param {String} macType
+ * @param {String} refIP
+ */
 Arpping.prototype.searchByMacType = function(macType, refIP) {
     macType = macType.toLowerCase();
     return this.discover(refIP).then(hosts => hosts.filter(h => h.type && h.type.toLowerCase() == macType));
